ac1f14bb674936de4e99b108d44459bbffca5813
ranranrahayu <ranran250880@gmail.com>
2021-03-15 01:05:05 +0700

    Nambah modul kirim email

---
+++ contactform.php
@@ -0,0 +1,18 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$subject = $_POST['phone'];
+	$mailfrom = $_POST['email'];
+	$message = $_POST['message'];
+
+	$mailTo = "ranran250880@gmail.com";
+	$header = "From: ".$emailfrom;
+	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+	mail($mailTo, $subject, $txt, $header);
+	header("location: Index.php?mailsend");
+
+}
+
+?>
